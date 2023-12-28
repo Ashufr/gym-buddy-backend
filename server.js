98657b5de6773be55d5ad527ad98e2225649161f
DrOreos <harashishzindabad@gmail.com>
2023-12-28 21:52:05 +0530

    Middleware order change

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/workouts", workoutRouter);
-app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
     console.log("Hello from server");
     res.json({msg: "Hello from server"})
 })
+app.use("/api/workouts", workoutRouter);
+app.use("/api/user", userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log("Server started on port " + process.env.PORT);
