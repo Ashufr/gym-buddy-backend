2b7799f9ebcfdc670e26d67a74bd3ee75f6fac94
DrOreos <harashishzindabad@gmail.com>
2023-12-29 15:35:22 +0530

    Password validation change

--- models/userModel.js
+++ models/userModel.js
@@ -34,17 +34,10 @@ userSchema.statics.signUp = async function (name, email, password) {
         throw new Error("Invalid email address format");
     }
 
-    if(!password.match(/^(?=(.*[a-z]){3,})(?=(.*[A-Z]){2,})(?=(.*[0-9]){2,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$/)){
+    if(password.length < 8 || !(/[a-z]/).test(password) || !(/[A-Z]/).test(password) || !(/[0-9]/).test(password) || !((/[!@#$%^&*()\-__+.]/).test(password))) {
         throw new Error("Password not Strong enough");
-        // ^                               start anchor
-        // (?=(.*[a-z]){3,})               lowercase letters. {3,} indicates that you want 3 of this group
-        // (?=(.*[A-Z]){2,})               uppercase letters. {2,} indicates that you want 2 of this group
-        // (?=(.*[0-9]){2,})               numbers. {2,} indicates that you want 2 of this group
-        // (?=(.*[!@#$%^&*()\-__+.]){1,})  all the special characters in the [] fields. The ones used by regex are escaped by using the \ or the character itself. {1,} is redundant, but good practice, in case you change that to more than 1 in the future. Also keeps all the groups consistent
-        // {8,}                            indicates that you want 8 or more
-        // $                               end anchor
     }
-
+    
     const exists = await this.findOne({ email });
     if (exists) {
         throw new Error("Email already exists");
